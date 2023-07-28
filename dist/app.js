f5a06c9a40a5a7cd65f780bcda216b091d3714f3
Iamthatnikhil <nikhilasssudani@gmail.com>
2023-07-29 02:12:06 +0530

    Code-update

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,31 @@
 "use strict";
 console.log("hello");
+let todo = document.getElementById("todoList");
+let taskInput = document.getElementById("taskInput");
+let addButton = document.getElementById("addButton");
+function createElement(task) {
+    HTMLElement;
+    {
+        const li = document.createElement("li");
+        li.textContent = task;
+        li.onclick = () => toggleCompletion(li);
+        return (li);
+    }
+    function addTodo() {
+        const task = taskInput.value.trim();
+        if (task === '')
+            alert("Kuch likh to lo bhai");
+        const li = createElement(task);
+        todoList.appendChild(li);
+        taskInput.value = "";
+    }
+    function toggleCompletion(todo) {
+        todo.style.textDecoration = todo.style.textDecoration === 'line-through' ? 'none' : 'line-through';
+    }
+    addButton.addEventListener('click', addTodo);
+    taskInput.addEventListener('keypress', (event) => {
+        if (event.key === 'Enter') {
+            addTodo();
+        }
+    });
+}
